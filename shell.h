74dcc516d725cc7a1b00050c5e9e49d0539e9635
Kb-Mash <karabo.kmashile@gmail.com>
2023-04-22 06:13:15 -0700

    header file update

--- shell.h
+++ shell.h
@@ -8,9 +8,13 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+/* environ variable */
+extern char **environ;
 
 /* Function Prototypes */
 void child_process(char *args[]);
+void exit_Builtin(char *cmd);
+void env_Builtin(void);
 int main(void);
 int args_line(char *entry, char **arguments);
 char *handle_path(char *dire_path, char *command);
