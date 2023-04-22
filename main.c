c5083276cd33a8d6bfb671f9066dd8fcae6d880b
Kb-Mash <karabo.kmashile@gmail.com>
2023-04-22 06:12:52 -0700

    add on for task 2-5

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 #include "shell.h"
 
-void child_process(char *args[]);
-
 /**
  * main - runs a simple shells
  * Return: 0 (success)
  */
 int main(void)
 {
-	char *cmd = NULL, *args[100];
+	char *cmd = NULL, *args[256];
 	size_t n = 0;
 	ssize_t len;
 
@@ -25,14 +23,16 @@ int main(void)
 		}
 
 		cmd[len - 1] = '\0';
-
-		/**
-		 * for (i = 0; i < 2; i++)
+		if (strcmp(cmd, "exit") == 0)
 		{
-			args[i] = cmd;
-			cmd = NULL;
+			exit_Builtin(cmd);
 		}
-		*/
+		else if (strcmp(cmd, "env") == 0 || strcmp(cmd, "printenv") == 0)
+		{
+			env_Builtin();
+			continue;
+		}
+
 		args_line(cmd, args);
 
 		child_process(args);
@@ -40,30 +40,4 @@ int main(void)
 
 	free(cmd);
 	return (0);
-}
-
-/**
- * child_process - creates a new process to execute the commands
- * @args: the array of pointers to arguments to execute
- */
-void child_process(char *args[])
-{
-	int status;
-	pid_t pid = fork();
-
-	if (pid == -1)
-	{
-		perror("Error");
-		exit(0);
-	}
-	else if (pid == 0)
-	{
-		execve(args[0], args, NULL);
-		perror("Error");
-		exit(0);
-	}
-	else
-	{
-		wait(&status);
-	}
 }
