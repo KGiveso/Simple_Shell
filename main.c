ff51253bbfec3a428520eff22c621b04857b45e3
KGiveso <mogoboyagiven@gmail.com>
2023-04-23 05:49:35 -0700

    task 6 and 7

--- main.c
+++ main.c
@@ -1,69 +1,88 @@
 #include "shell.h"
-
+char *handle_path(char *dire_path, char *command);
 void child_process(char *args[]);
-
 /**
- * main - runs a simple shells
- * Return: 0 (success)
- */
+* main - runs a simple shells
+* Return: 0 (success)
+*/
 int main(void)
 {
-	char *cmd = NULL, *args[100];
-	size_t n = 0;
-	ssize_t len;
+char *cmd = NULL, *args[100];
+size_t n = 0;
+ssize_t len;
+while (1)
+{
+printf("$ ");
+fflush(stdout);
 
-	while (1)
-	{
-		printf("$ ");
-		fflush(stdout);
+len = getline(&cmd, &n, stdin);
+if (len == -1)
+{
+printf("\n");
+break;
+}
 
-		len = getline(&cmd, &n, stdin);
-		if (len == -1)
-		{
-			printf("\n");
-			break;
-		}
+cmd[len - 1] = '\0';
 
-		cmd[len - 1] = '\0';
+args_line(cmd, args);
 
-		/**
-		 * for (i = 0; i < 2; i++)
-		{
-			args[i] = cmd;
-			cmd = NULL;
-		}
-		*/
-		args_line(cmd, args);
+child_process(args);
+}
 
-		child_process(args);
-	}
-
-	free(cmd);
-	return (0);
+free(cmd);
+return (0);
 }
 
 /**
- * child_process - creates a new process to execute the commands
- * @args: the array of pointers to arguments to execute
- */
+* child_process - creates a new process to execute the commands
+* @args: the array of pointers to arguments to execute
+*/
 void child_process(char *args[])
 {
-	int status;
-	pid_t pid = fork();
+int status;
+pid_t pid = fork();
 
-	if (pid == -1)
-	{
-		perror("Error");
-		exit(0);
-	}
-	else if (pid == 0)
-	{
-		execve(args[0], args, NULL);
-		perror("Error");
-		exit(0);
-	}
-	else
-	{
-		wait(&status);
-	}
+if (pid == -1)
+{
+perror("Error");
+exit(0);
 }
+else if (pid == 0)
+{
+char *path = handle_path(getenv("PATH"), args[0]);
+if (path == NULL)
+{
+printf("%s: command not found\n", args[0]);
+exit(0);
+}
+execve(path, args, NULL);
+perror("Error");
+exit(0);
+}
+else
+{
+wait(&status);
+}
+}
+/**
+* handle_path - looks for a command in the directories of the PATH
+* @dire_path: the string containing the PATH
+* @command: the command to look for
+* Return: the path to the command if found, NULL otherwise
+*/
+char *handle_path(char *dire_path, char *command)
+{
+char *path = strtok(dire_path, ":");
+
+while (path != NULL)
+{
+char *full_path = malloc(strlen(path) + strlen(command) + 2);
+sprintf(full_path, "%s/%s", path, command);
+if (access(full_path, F_OK | X_OK) != -1)
+return (full_path);
+free(full_path);
+path = strtok(NULL, ":");
+}
+return (NULL);
+}
+
--- shell.h
+++ shell.h
@@ -10,10 +10,20 @@
 
 
 /* Function Prototypes */
+/**Task 0 and 1**/
 void child_process(char *args[]);
 int main(void);
+
+/**Task 2 and 3 */
 int args_line(char *entry, char **arguments);
 char *handle_path(char *dire_path, char *command);
 
-#endif
+/**Task 4 and 5*/
+void exit_Builtin(char *cmd);
+void child_process(char *args[]);
 
+/**Task 6 and 7*/
+char *get_static(void);
+char *_strdup(char *strt);
+
+#endif /*SHELL_H*/
