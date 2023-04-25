d7d9ae3edc28825e90cf6879e9baa52ce0a2e465
KGiveso <mogoboyagiven@gmail.com>
2023-04-25 10:21:25 -0700

    mandatory_tasks

--- path.c
+++ path.c
@@ -1,38 +1,91 @@
 #include "shell.h"
+
 /**
-* handle_path - Concatenates an input with paths in global variable PATH
-* @dire_path: concatenates as the directory string with the command.
-* @command: command to be concatenated with the directory
-* Return: Buffer to concatenated path
-*/
-char *handle_path(char *dire_path, char *command)
-{
-int a, b = 0, len1, len2;
-char *command_path = NULL;
+ * _path - handles the PATH
+ * @cmd: command read from input
+ * Description: fork must not be called if the command doesn’t exist
+ * Return:
+ */
 
-if (dire_path == NULL || command == NULL)
-return (NULL);
+char *_path(char *cmd)
+{
+	char *path, *full_path, *token;
+	size_t path_len, cmd_len;
+	struct strcat st = {0};
 
-len1 = strlen(dire_path);
-len2 = strlen(command);
-command_path = malloc(len1 + len2 + 2);
-if (command_path == NULL)
-return (NULL);
-for (a = 0; dire_path[a] != '\0'; a++)
-{
-command_path[a] = dire_path[a];
-}
-if (dire_path[a - 1] != '/')
-{
-command_path[a] = '/';
-a++;
-}
-while (command[b] != '\0')
-{
-command_path[a + b] = command[b];
-b++;
-}
-command_path[a + b] = '\0';
-return (command_path);
+	path = getenv("PATH");
+	if (path)
+	{
+		cmd_len = strlen(cmd);
+
+		token = strtok(path, ":");
+		while (token != NULL)
+		{
+			path_len = strlen(token);
+			full_path = malloc(cmd_len + path_len + 2);
+			strcpy(full_path, token);
+			strcat(full_path, "/");
+			strcat(full_path, cmd);
+
+			if (strcat(full_path, &st) == 0)
+			{
+				return (full_path);
+			}
+			else
+			{
+				free(full_path);
+				token = strtok(NULL, ":");
+
+			}
+		}
+
+		if (strcat(cmd, &st) == 0)
+		{
+			return (cmd);
+		}
+		return (NULL);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
 
+
+/**
+ * child_process - creates a new process to execute the commands
+ * @args: the array of pointers to arguments to execute
+ */
+void child_process(char *args[])
+{
+	int status;
+	pid_t pid;
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Error");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		char *cmd, *full_path;
+
+		cmd = args[0];
+		full_path = malloc(1024);
+
+		full_path = _path(cmd);
+
+		execve(full_path, args, NULL);
+		perror("Error");
+
+		free(full_path);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(&status);
+
+	}
+}
