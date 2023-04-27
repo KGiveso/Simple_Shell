a67f26ee17664e52d0db1a06e9fcd43c921eaa42
Kb-Mash <karabo.kmashile@gmail.com>
2023-04-27 05:48:59 -0700

    creates a ne process to execute commands

--- execute.c
+++ execute.c
@@ -4,9 +4,13 @@
  * child_process - creates a new process to execute the command
  * @args: array of pointers to command line
  */
-void child_process(char *args[100])
+void child_process(char **args)
 {
-	int status;
+	char *full_path = malloc(sizeof(char) * 1024);
+
+	if (full_path == NULL)
+		exit(EXIT_FAILURE);
+
 	pid_t pid = fork();
 
 	if (pid == -1)
@@ -15,10 +19,18 @@ void child_process(char *args[100])
 	}
 	else if (pid == 0)
 	{
-		if (execve(args[0], args, NULL) == -1)
+		full_path = _path(args[0]);
+
+		if (args[0][0] == '/' || full_path == NULL)
+			full_path = _strdup(args[0]);
+
+		if (execve(full_path, args, NULL) == -1)
 			perror(args[0]);
+
 		exit(EXIT_SUCCESS);
 	}
 	else
-		wait(&status);
+		wait(NULL);
+
+	free(full_path);
 }
