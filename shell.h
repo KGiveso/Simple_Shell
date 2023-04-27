4bf07a32fdc52450216ed2e8badccc3d40ec5472
Kb-Mash <karabo.kmashile@gmail.com>
2023-04-27 05:45:10 -0700

    header file

--- shell.h
+++ shell.h
@@ -2,14 +2,25 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
+/* global variable */
 extern char **environ;
 
-void child_process(char *argv[]);
+/* function prototypes */
+char **tokenize(char *cmd, char *delim, int *tokens);
+char *_path(char *cmd);
+void child_process(char **args);
+void _free(char **args);
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *s1);
+void exit_env(char *cmd);
 
 #endif
